0e4313539094478501607e704cbdd0115ff213e0
Denzell Robyn Dy <dqdy@up.edu.ph>
2023-08-29 10:47:13 +0800

    Construct skeleton files

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readYet) {
+  // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readYet = readYet;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+
+}
